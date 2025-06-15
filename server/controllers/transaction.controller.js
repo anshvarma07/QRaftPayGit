f190533baee7554680635154feda8b936de93652
Ansh Varma <ansh.varma7@gmail.com>
2025-06-15 16:07:56 +0530

    Get Transactions for Specific Vendor

--- server/controllers/transaction.controller.js
+++ server/controllers/transaction.controller.js
@@ -8,6 +8,25 @@ exports.getTransactions = async (req, res) => {
   res.json({ success: true, data: transactions });
 };
 
+exports.getTransactionsByVendor = async (req, res) => {
+  try {
+    const { vendorId } = req.params;
+
+    if (!vendorId) {
+      return res.status(400).json({ success: false, message: 'Vendor ID is required' });
+    }
+
+    const transactions = await Transaction.find({ vendorId }).populate('vendorId buyerId');
+
+    return res.json({ success: true, data: transactions });
+  } catch (error) {
+    console.error('Error fetching transactions by vendor:', error);
+    return res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
+
+
+
 exports.createTransaction = async (req, res) => {
   const { vendorId, amount, remarks, qrCode } = req.body;
   const buyerId = req.user.id;
--- server/routes/transaction.routes.js
+++ server/routes/transaction.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const auth = require('../middleware/auth');
-const { getTransactions, createTransaction } = require('../controllers/transaction.controller');
+const { getTransactions, createTransaction,getTransactionsByVendor } = require('../controllers/transaction.controller');
 const router = express.Router();
 
 router.get('/', auth, getTransactions);
+router.get('/vendor/:vendorId', auth, getTransactionsByVendor);
 router.post('/', auth, createTransaction);
 
 module.exports = router;
